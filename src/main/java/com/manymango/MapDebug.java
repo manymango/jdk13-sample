678cfb8151fd86cb913faa0ac52c93b2b00c9f13
manymango.com <964665651@qq.com>
2020-01-05 14:15:20 +0800

    xxx

---
+++ src/main/java/com/manymango/MapDebug.java
@@ -0,0 +1,11 @@
+package com.manymango;
+
+/**
+ * @author manymango
+ * @date 2019/11/2 23:56
+ */
+public class MapDebug {
+    public static void main(String[] args) {
+        System.out.println("xixi");
+    }
+}
